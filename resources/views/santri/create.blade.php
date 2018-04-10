a7cae7a9105a2a1f905fd43edc2d0c3baa22255d
sheenazien8 <sheenazien08@gmail.com>
2018-04-10 14:30:19 +0700

    validation and feedback user2

--- resources/views/santri/create.blade.php
+++ resources/views/santri/create.blade.php
@@ -7,22 +7,22 @@
 
   <input type="text" name="nama" placeholder="Masukkan Namamu"><br>
   @if ($errors->has('nama'))
-    <strong>{{ $errors->first('nama')}}</strong>
+    <strong>{{ $errors->first('nama')}}</strong> <br>
   @endif
 
   <input type="text" name="umur" placeholder="Masukkan umur"><br>
   @if ($errors->has('umur'))
-    <strong>{{ $errors->first('umur')}}</strong>
+    <strong>{{ $errors->first('umur')}}</strong> <br>
   @endif
 
   <textarea name="alamat" placeholder="Masukkan alamat"></textarea><br>
   @if ($errors->has('alamat'))
-    <strong>{{ $errors->first('alamat')}}</strong>
+    <strong>{{ $errors->first('alamat')}}</strong> <br>
   @endif
 
   <input type="text" name="jenis_kelamin" placeholder="jenis kelamin"><br>
   @if ($errors->has('jenis_kelamin'))
-    <strong>{{ $errors->first('jenis_kelamin')}}</strong>
+    <strong>{{ $errors->first('jenis_kelamin')}}</strong> <br>
   @endif
 
   <input type="submit" name="submit" value="Submit">
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('santri/create', 'SantriController@index')->name('santri.index');
+
 Route::get('santri/create', 'SantriController@create')->name('santri.create');
 
 Route::post('santri', 'SantriController@store')->name('santri.store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
